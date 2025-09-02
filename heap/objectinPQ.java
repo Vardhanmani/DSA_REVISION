d764ea89a292c8ad1373e603a40885fe8d8c1586
mani <pashammanl2@gmail.com>
2025-09-03 04:06:42 +0530

    Heap Object in ascending order

---
+++ heap/objectinPQ.java
@@ -0,0 +1,29 @@
+import java .util.*;
+public class objectinPQ {
+    static class Student implements Comparable<Student> {
+       String name;
+       int rank;
+    public Student(String name,int rank){
+       this.name=name;
+       this.rank=rank;
+    }
+       @Override
+    public int compareTo(Student s2){
+       return this.rank-s2.rank;
+   }
+}
+public static void main(String[] args) {
+    PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("a", 4));
+        pq.add(new Student("b", 3));
+        pq.add(new Student("c", 5));
+        pq.add(new Student("d", 1));
+        pq.add(new Student("e", 2));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " "+ pq.peek().rank);
+            pq.remove();
+        }
+}
+    
+}
