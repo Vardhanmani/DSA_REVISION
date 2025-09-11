7edd4fce7c00e941b35bd820e080e9c724bfe4b2
mani <pashammanl2@gmail.com>
2025-09-11 20:28:02 +0530

    Create hashing

---
+++ hashing/createhashmap.java
@@ -0,0 +1,13 @@
+import java .util.*;
+public class createhashmap{
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm=new HashMap<>();
+        //add 
+        hm.put("bankai", 100);
+        hm.put("arise", 80);
+        hm.put("ego", 100);
+        hm.put("shadow", 10);
+
+        System.out.println(hm +" ->");
+    }
+}
