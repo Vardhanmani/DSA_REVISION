10692db385127bf53734c24ab37d4a8e065e7df0
mani <pashammanl2@gmail.com>
2025-09-12 02:56:43 +0530

    TreeMap in hashing

---
+++ hashing/TreeMapinhash.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class TreeMapinhash {
+   public static void main(String[] args) {
+    TreeMap<String,Integer>tm=new TreeMap<>();
+    tm.put("mani", 100);
+    tm.put("chinu", 100);
+    tm.put("shidou", 100);
+    tm.put("ramya", 100);
+
+    System.out.println(tm);
+   } 
+}
