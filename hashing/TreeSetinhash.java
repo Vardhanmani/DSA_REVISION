1c1241c582bd3420bf54c175ec958c4558a828bc
mani <pashammanl2@gmail.com>
2025-09-13 16:49:22 +0530

    TreeSet in Hash

---
+++ hashing/TreeSetinhash.java
@@ -0,0 +1,12 @@
+import java .util.*;
+public class TreeSetinhash {
+    public static void main(String[] args) {
+        TreeSet<String>ts=new TreeSet<>();
+        ts.add("ma");
+   ts.add("ni");
+   ts.add("vard");
+   ts.add("han");
+
+   System.out.println(ts);
+    }
+}
