9bd3a30957c34481958ecfb010c5803e07714db6
mani <pashammanl2@gmail.com>
2025-09-13 16:41:52 +0530

    Linked HashSet

---
+++ hashing/Linkedhashset.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Linkedhashset {
+    public static void main(String[] args) {
+   LinkedHashSet<String>lhs=new LinkedHashSet<>();
+   lhs.add("ma");
+   lhs.add("ni");
+   lhs.add("vard");
+   lhs.add("han");
+
+   System.out.println(lhs);
+}
+}
