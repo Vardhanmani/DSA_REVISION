be7dedea40c20d531394f633d43e5a798aa3aabf
mani <pashammanl2@gmail.com>
2025-09-14 22:47:36 +0530

    Largest Subarray sum =0  in Hash

---
+++ hashing/largestsubarray.java
@@ -0,0 +1,19 @@
+import java .util.*;
+public class largestsubarray {
+    public static void main(String[] args) {
+        int arr[]={15,-2,2,-8,1,7,10,23};
+        HashMap<Integer ,Integer> map = new HashMap<>();
+        int sum=0;
+        int length=0;
+        for(int j=0;j<arr.length;j++){
+            sum+=arr[j];
+            if(map.containsKey(sum)){
+                length=Math.max(length, j-map.get(sum));
+
+            }else{
+                map.put(sum, j);
+            }
+        }
+        System.out.println(length);
+    }
+}
