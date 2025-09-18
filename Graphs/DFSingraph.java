af99aa2bd9384464e5916ebb38fdc2641a6401cd
mani <pashammanl2@gmail.com>
2025-09-19 02:42:34 +0530

    DFS in Graph

---
+++ Graphs/DFSingraph.java
@@ -0,0 +1,59 @@
+import java . util.*;
+public class DFSingraph {
+       static class Edge {
+   int scr;
+   int dest;
+  
+
+   public Edge(int scr,int dest){
+    this.scr=scr;
+    this.dest=dest;
+   
+   }
+    
+   }
+    static void creategraph(ArrayList<Edge> graph[]){
+    for(int i=0; i<graph.length; i++){
+       graph[i]=new ArrayList<>();
+    }
+    graph[0].add(new Edge(0, 1));
+    graph[0].add(new Edge(0, 2));
+
+    graph[1].add(new Edge(1, 0));
+    graph[1].add(new Edge(1, 3));
+
+    graph[2].add(new Edge(2, 0));
+    graph[2].add(new Edge(2, 4));
+
+    graph[3].add(new Edge(3, 1));
+    graph[3].add(new Edge(3, 4));
+    graph[3].add(new Edge(3, 5));
+
+    graph[4].add(new Edge(4, 2));
+    graph[4].add(new Edge(4, 3));
+    graph[4].add(new Edge(4, 5));
+
+    graph[5].add(new Edge(5, 3));
+    graph[5].add(new Edge(5, 4));
+    graph[5].add(new Edge(5, 6));
+
+    graph[6].add(new Edge(6, 5));
+    }
+
+    public static void DFS(ArrayList<Edge> graph[],int curr ,boolean visit[]){
+        System.out.println(curr);
+        visit[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if(!visit[e.dest]){
+                DFS(graph,e.dest,visit);
+            }
+        }
+    }
+       public static void main(String[] args) {
+    int v=7;
+    ArrayList<Edge> graph []=new ArrayList[v];
+    creategraph(graph);
+    DFS(graph, 0, new boolean[v]);
+       }
+}
