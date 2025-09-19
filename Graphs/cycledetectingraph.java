e626707a8e32193d71685a3e26da6b2d4ba55c20
mani <pashammanl2@gmail.com>
2025-09-19 21:25:25 +0530

    Cycle Detect in graph

---
+++ Graphs/cycledetectingraph.java
@@ -0,0 +1,79 @@
+import java .util.*;
+public class cycledetectingraph {
+        
+   static class Edge {
+   int scr;
+   int dest;
+  
+
+   public Edge(int scr,int dest){
+    this.scr=scr;
+    this.dest=dest;
+   
+   }
+    
+   }
+    static void creategraph(ArrayList<Edge> graph[]){
+    for(int i=0; i<graph.length; i++){
+       graph[i]=new ArrayList<>();
+    }
+    graph[0].add(new Edge(0, 1));
+    graph[0].add(new Edge(0, 2));
+
+    graph[1].add(new Edge(1, 0));
+    graph[1].add(new Edge(1, 3));
+
+    graph[2].add(new Edge(2, 0));
+    graph[2].add(new Edge(2, 4));
+
+    graph[3].add(new Edge(3, 1));
+    graph[3].add(new Edge(3, 4));
+    graph[3].add(new Edge(3, 5));
+
+    graph[4].add(new Edge(4, 2));
+    graph[4].add(new Edge(4, 3));
+    graph[4].add(new Edge(4, 5));
+
+    graph[5].add(new Edge(5, 3));
+    graph[5].add(new Edge(5, 4));
+    graph[5].add(new Edge(5, 6));
+
+    graph[6].add(new Edge(6, 5));
+  
+   }
+
+   public static boolean detectcycle(ArrayList<Edge>[] graph){
+    boolean visit[]=new boolean[graph.length];
+for(int i=0;i<graph.length;i++){
+    if(!visit[i]){
+        if(detectcycleutil(graph,visit,i,-1)){
+            return true;
+        }
+    }
+}
+return false;
+
+   }
+
+   public static boolean detectcycleutil(ArrayList<Edge>[]graph,boolean visit[],int curr,int parent){
+    visit[curr]=true;
+    for(int i=0;i<graph[curr].size();i++){
+        Edge e= graph[curr].get(i);
+
+        //case  3
+        if(!visit[e.dest]&&detectcycleutil(graph, visit, e.dest, curr)){
+            return true;
+        }
+        else if(visit[e.dest] && e.dest != parent){
+            return true;
+        }
+    }
+    return false;
+   }
+   public static void main(String[] args) {
+    int v=7;
+    ArrayList<Edge> graph []=new ArrayList[v];
+    creategraph(graph);
+   System.out.println(detectcycle(graph));
+}
+}
