b1f7f0b2a18c160331a8affe2f8af091fef6623c
mani <pashammanl2@gmail.com>
2025-09-29 00:59:09 +0530

    Matrixchainmultiplication in (DP)

---
+++ Dynamic programming (dp)/Matrixchainmultiplication.java
@@ -0,0 +1,23 @@
+import java .util.*;
+public class Matrixchainmultiplication {
+    public static int mcm(int arr[],int i,int j){//i==starting point j=endingpoint
+        //base case
+        if(i==j){
+            return 0;
+        }
+        int ans =Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++){
+            int cost1=mcm(arr, i, k);//Ai..Ak  arr[i-1]*arr[k] result
+            int cost2=mcm(arr, k+1, j);//Ak..Aj arr[k]*arr[j] result
+            int cost3=arr[i-1]*arr[k]*arr[j];
+            int finalcost=cost1+cost2+cost3;
+            ans = Math.min(ans, finalcost);
+        }
+        return ans;
+    } 
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,3};
+        int n=arr.length;
+        System.out.println(mcm(arr, 1, n-1));
+    }   
+}
