79670a716089268f52063b32986f7476fad3958f
Vardhanmani <pashammanl2@gmail.com>
2026-07-21 23:40:38 -0700

    day2

---
+++ arraysortt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class arraysortt {
+    public static void main (String args[]){
+        int arr[] = {1,2,3,4,5};
+        boolean issorted= true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                issorted = false;
+                break;
+            }
+        }
+        System.out.println(issorted);
+    }
+}
