15c094d97ede865e0a7deccd1993faf164d600bd
Vardhanmani <pashammanl2@gmail.com>
2026-07-23 03:10:14 -0700

    day2

---
+++ arrayList.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class arrayList {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>(4);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.add(5);
+        System.out.println(list.get(1));
+        list.remove(3);
+        System.out.println(list.size());
+        System.out.print(list.get(3));
+        
+    }
+}
