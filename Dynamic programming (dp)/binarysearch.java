d037fc8ba45690e480d33d68140925ad4d957b4b
Vardhanmani <pashammanl2@gmail.com>
2026-08-01 12:22:06 +0530

    Day11

---
+++ Dynamic programming (dp)/binarysearch.java
@@ -0,0 +1,36 @@
+public class binarysearch {
+    public static int search(int[] arr, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[start] <= arr[mid]) {
+            if (target >= arr[start] && target < arr[mid]) {
+                return search(arr, target, start, mid - 1); 
+            } else {
+                return search(arr, target, mid + 1, end);   
+            }
+        } 
+        else {
+            if (target > arr[mid] && target <= arr[end]) {
+                return search(arr, target, mid + 1, end);   
+            } else {
+                return search(arr, target, start, mid - 1); 
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int index = search(arr, target, 0, arr.length - 1);
+        if (index != -1) {
+            System.out.println("Target " + target + " found at index: " + index);
+        } else {
+            System.out.println("Target " + target + " not found in array.");
+        }
+    }
+}
