ca9dd01cd0619b8c04b8f30fcff7a5d61f8a6f1c
Vardhanmani <pashammanl2@gmail.com>
2026-08-04 17:59:44 +0530

    Day14

--- Recursion.java
+++ Recursion.java
@@ -1,9 +1,13 @@
 import java .util.*;
 public class Recursion {
     public static void printCountdown(int n) {
-    for (int i = n; i >= 1; i--) {
-        System.out.println(i);
+        //basecase
+    if(n==0){
+        return;
     }
+    
+    printCountdown(n-1);
+    System.out.print(n);
 }
   public static void main(String[] args){
     int n=9;
