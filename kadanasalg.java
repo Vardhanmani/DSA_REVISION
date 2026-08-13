d960a883ffd28a64c4c1bc42e57d3359667c09d5
Vardhanmani <pashammanl2@gmail.com>
2026-08-13 17:31:05 +0530

    Day24

--- kadanasalg.java
+++ kadanasalg.java
@@ -21,6 +21,6 @@ public class kadanasalg{
 
     public static void main(String[] args) {
         int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println("Maximum Subarray Sum is: " + maxSubarraySum(arr)); // Output: 6
+        System.out.println("Maximum Subarray Sum is: " + maxSubarraySum(arr));
     }
 }
