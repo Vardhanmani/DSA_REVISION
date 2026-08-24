51a2c15a0b961b71e094149e524601d930f42fb6
Vardhanmani <pashammanl2@gmail.com>
2026-08-24 12:48:24 +0530

    Day33

---
+++ diamater.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class diamater {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data= data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static int maxs ;
+    public static int diamaters(Node root){
+        //base case
+        if(root == null){
+            return 0;
+        }
+        int left = diamaters(root.left);
+        int right = diamaters(root.right);
+        int current = left+right;
+        maxs = Math.max(maxs,current);
+        return Math.max(left,right)+1;
+    }
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        diamaters(root);
+        System.out.print(maxs);
+    }
+}
