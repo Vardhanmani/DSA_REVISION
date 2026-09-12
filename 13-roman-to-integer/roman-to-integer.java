cb81faae18ce30cfe9480180b7cde163d7409fcd
Vardhanmani <pashammanl2@gmail.com>
2026-09-12 21:42:56 +0530

    Time: 3 ms (76.61%) | Memory: 46.6 MB (48.01%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,31 +1,15 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
-
-
-        int sum = 0;
-        int prev = 0;
-
-        for(int i=s.length()-1; i>=0; i--)
-        {
-            int curr = map.get(s.charAt(i));
-            if(curr < prev)
-            {
-                sum -= curr;
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int ans = 0;
+        
+        for (int i = 0; i < symbols.length; i++) {
+            while (s.startsWith(symbols[i])) {
+                ans += values[i];
+                s = s.substring(symbols[i].length());
             }
-            else
-            {
-                sum += curr;
-            }
-            prev = curr;
         }
-        return sum;
+        return ans;
     }
 }
