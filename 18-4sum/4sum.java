1683cdb827010fd4a3c6b34fc6a4ec1d6eda8dba
Vardhanmani <pashammanl2@gmail.com>
2026-09-14 15:37:31 +0530

    Time: 22 ms (20.30%) | Memory: 46 MB (20.84%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,41 +1,38 @@
-import java.util.Arrays;
 class Solution {
-    public List<List<Integer>> fourSum(int[] n, int target) {
-          List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (arr == null || arr.length < 4) return list;
         
-        Arrays.sort(n);
-        for(int i=0;i<=n.length-1;i++){
-            if(i>0 && n[i] == n[i-1]){
-                continue;
-            }
-            for(int j=i+1;j<=n.length-1;j++){
-                 if (j > i + 1 && n[j] == n[j - 1]){
-                    continue;
-                }    
-                int k=j+1;
-                int m=n.length-1;
-                while (k<m) {
-                     long sum = (long)n[i] + n[j] + n[k] + n[m];
-                    if(sum == target){
-                        ans.add(Arrays.asList(n[i],n[j],n[k],n[m]));
-                         while (k<m && n[k] == n[k+1]) {
-                        k++;
-                    }
-                    while (k<m && n[m] == n[m-1]) {
-                        m--;
-                    }
-                    k++;
-                    m--;
-                    }
-                    else if (sum>target) {
-                        m--;
-                    }else{
-                        k++;
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < arr.length - 3; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue; 
+            
+            for (int j = i + 1; j < arr.length - 2; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+                
+                int left = j + 1;
+                int right = arr.length - 1;
+                
+                while (left < right) {
+                    long sum = (long) arr[i] + arr[j] + arr[left] + arr[right]; 
+                    
+                    if (sum == target) {
+                        list.add(Arrays.asList(arr[i], arr[j], arr[left], arr[right]));
+                        left++;
+                        right--;
+                        
+                        while (left < right && arr[left] == arr[left - 1]) left++; 
+                        while (left < right && arr[right] == arr[right + 1]) right--; 
+                        
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
                     }
                 }
             }
-        }    
-        return ans;
-
+        }
+        return list;
     }
 }
